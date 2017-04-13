8a45d3f0542891c50ec50275f105289066962bbd
Brian Morgan <brian.s.morgan@gmail.com>
2017-04-13 13:34:29 -0400

    add in timestamp to page

--- sample-app/hello.py
+++ sample-app/hello.py
@@ -1,10 +1,12 @@
+import datetime
+
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! {}".format(datetime.utcnow())
 
 
 @app.route("/health/")
